e192e157c851f6190b759672e9e7cbc7c2ce5571
Othmane Hanyf <othmane.hanyf@protonmail.com>
2021-02-14 00:12:57 +0100

    second asm algo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Basic algorithms implelemented in x86 assembly language (__intel syntax__):
-* Transfer elements from one array to another _(array_transfer.asm)_
+* Transfer elements from one array to another _(array\_transfer.asm)_
+* Find minimum and maximum of an array _(min\_max\_array.asm)_
---
+++ min_max_array.asm
@@ -0,0 +1,73 @@
+;----------------------------------------
+segment .data
+    tab     db      2, 3, 2, 9, 3, 5, 6, 5 ,8 , 1
+    min     db      0
+    max     db      0
+    endl    db      10
+;----------------------------------------
+segment .bss
+;----------------------------------------
+segment .text
+global _start
+_start:
+    mov     esi, 0
+    mov     al, byte [tab]
+    mov     byte [min], al
+    mov     byte [max], al
+; Main loop
+label1:
+    inc     esi
+    mov     al, byte[tab+esi]
+    cmp     byte [min], al 
+    jnb     label_min
+label2:
+    cmp     byte [max], al
+    jc      label_max
+label_cont:
+    cmp     esi, 10
+    jb      label1
+    jmp     output
+
+; execute this if the array item is less than the current minimum
+label_min:
+    mov     byte [min], al
+    jmp     label2
+
+; execute this if the array item is greater than the current maximum
+label_max:
+    mov     byte [max], al
+    jmp     label_cont
+
+; Print out the results
+output:
+    ; Minimum 
+    add     byte[min], 48
+    mov     ecx, min
+    mov     edx, 1
+    call    print
+    ; New line
+    mov     ecx, endl
+    mov     edx, 1
+    call    print
+    ; Maximum 
+    add     byte[max], 48
+    mov     ecx, max
+    mov     edx, 1
+    call    print
+    ; New line
+    mov     ecx, endl
+    mov     edx, 1
+    call    print
+
+exit_program:
+    mov     eax, 1
+    mov     ebx, 0
+    int     80h
+    ret
+
+global print
+print:
+    mov     eax, 4
+    mov     ebx, 1
+    int     80h
+    ret
